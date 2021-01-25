f6e0d2343b219ce8f4a99fa56eef50430b56ad80
Julian Snelders <j.snelders@outlook.com>
2021-01-25 13:25:04 +0100

    map layout

---
+++ Assets/scripts/GameManagement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManagement : MonoBehaviour
+{
+ public void HallWay()
+    {
+        SceneManager.LoadScene("HallWay");
+    }
+
+    public void ClassRoom1()
+    {
+        SceneManager.LoadScene("ClassRoom1");
+    }
+
+    public void ClassRoom2()
+    {
+        SceneManager.LoadScene("ClassRoom2");
+    }
+
+    public void ClassRoom3()
+    {
+        SceneManager.LoadScene("ClassRoom3");
+    }
+
+    public void FrontSchool()
+    {
+        SceneManager.LoadScene("FrontSchool");
+    }
+    public void AlbertH()
+    {
+        SceneManager.LoadScene("AlbertH");
+    }
+
+}
